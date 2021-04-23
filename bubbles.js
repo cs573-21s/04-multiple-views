0a9b924ce62069fa01d2d62d79ef624918f45c9a
imogencs <Imogen.ics1@gmail.com>
2021-04-23 01:05:47 -0400

    fmat

--- bubbles.js
+++ bubbles.js
@@ -26,21 +26,35 @@ var pack = d3.pack()
 
 
 var nest = d3.nest()
-    .key(function (d) { return d.Kingdom; })
-    .key(function (d) { return d.Phylum; })
+    .key(function (d) {
+        return d.Kingdom;
+    })
+    .key(function (d) {
+        return d.Phylum;
+    })
     .key(function (d) { return d.Class; })
     .key(function (d) { return d.Over; })
-    .key(function (d) { return d.Family; })
-    .key(function (d) { return d.Genus; })
-    .key(function (d) { return d.Species; })
+
+    .key(function (d) {
+        console.log(d.Family)
+        return d.Family;
+    })
+    .key(function (d) {
+        console.log(d.Genus)
+        return d.Genus;
+    })
+    .key(function (d, i) {
+        console.log(d.Species, i)
+        return d.Species;
+    })
     .rollup(function (d) { return d3.sum(d, function (d) { return d.Species; }); });
 
-var treemap = d3.treemap()
-    .size([width, height])
-    .padding(1)
-    .round(true);
+// var treemap = d3.treemap()
+//     .size([width, height])
+//     .padding(1)
+//     .round(true);
 
-d3.csv("https://raw.githubusercontent.com/imogencs/04-multiple-views/main/animals.csv", function (data) {
+d3.csv("https://raw.githubusercontent.com/imogencs/04-multiple-views/main/simple_reptiles.csv", function (data) {
     // console.log(data.columns)
 
 
@@ -48,25 +62,32 @@ d3.csv("https://raw.githubusercontent.com/imogencs/04-multiple-views/main/animal
         .sum(function (d) { return d.value; })
         .sort(function (a, b) { return b.value - a.value; });
 
-    treemap(root);
+    // treemap(root);
 
-    console.log(data)
-    // console.log(data.value)
-    var root = stratify(data)
-        .sum(function (d) {
-            console.log(d.value)
-            return d.value;
-        })
-        .sort(function (a, b) { return b.value - a.value; });
-    console.log(root)
+    // var root = stratify(data)
+    //     .sum(function (d) { return d.value; })
+    //     .sort(function (a, b) { return b.value - a.value; });
 
     pack(root);
+
+
+    // console.log(data)
+    // // console.log(data.value)
+    // var root = stratify(data)
+    //     .sum(function (d) {
+    //         console.log(d.value)
+    //         return d.value;
+    //     })
+    //     .sort(function (a, b) { return b.value - a.value; });
+    // console.log(root)
+
+    // pack(root);
 
     var node = svg.select("g")
         .selectAll("g")
         .data(root.descendants())
         .enter().append("g")
-        .attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; })
+        // .attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; })
         .attr("class", function (d) { return "node" + (!d.children ? " node--leaf" : d.depth ? "" : " node--root"); })
         .each(function (d) { d.node = this; })
         .on("mouseover", hovered(true))
@@ -74,7 +95,8 @@ d3.csv("https://raw.githubusercontent.com/imogencs/04-multiple-views/main/animal
 
     node.append("circle")
         .attr("id", function (d) { return "node-" + d.class; })
-        .attr("r", function (d) { return d.r; })
+        // .attr("r", function (d) { return d.r; })
+        .attr("r", 20)
         .style("fill", function (d) { return color(d.depth); });
 
     var leaf = node.filter(function (d) { return !d.children; });
@@ -87,7 +109,10 @@ d3.csv("https://raw.githubusercontent.com/imogencs/04-multiple-views/main/animal
     leaf.append("text")
         .attr("clip-path", function (d) { return "url(#clip-" + d.class + ")"; })
         .selectAll("tspan")
-        .data(function (d) { return d.class.substring(d.class.lastIndexOf(".") + 1).split(/(?=[A-Z][^A-Z])/g); })
+        .data(function (d) {
+            console.log(d.class)
+            return d.class.substring(d.class.lastIndexOf(".") + 1).split(/(?=[A-Z][^A-Z])/g);
+        })
         .enter().append("tspan")
         .attr("x", 0)
         .attr("y", function (d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
