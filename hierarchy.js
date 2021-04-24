b7c385b308ba8b109593a2d99e736c776890d0e0
imogencs <Imogen.ics1@gmail.com>
2021-04-23 20:28:38 -0400

    jason

--- hierarchy.js
+++ hierarchy.js
@@ -1,95 +1,3 @@
-// var margin = {top: 0, bottom: 80, left: 50, right: 80};
-// var width = 960;
-// var height = 500;
-
-// // Generate svg
-// var g = d3.select('body').append('svg')
-//     .attr('width', width)
-//     .attr('height', height)
-//     .append('g')
-//     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-//     .attr('width', width - margin.left - margin.right)
-//     .attr('height', height - margin.top - margin.bottom);
-
-// // Create cluster
-// var cluster = d3.cluster()
-//   .size([height - margin.bottom - margin.top, width - margin.right - margin.left]);
-
-// // Get data
-// d3.json("https://raw.githubusercontent.com/imogencs/04-multiple-views/main/animals2.json", function(error, data) {
-
-//   if (error) throw error;
-
-//   // If data is already in a hierarchical form
-//   // such as json, it can directly be passed
-//   // into to d3.hierarchy. If not
-//   // use d3.stratify
-//   var root = d3.hierarchy(data);
-
-//   // By assigning the root to cluster
-//   // the x and y values are computed
-//   cluster(root);
-
-//   // Add links between the nodes
-//   var link = g.selectAll(".link")
-//       // root.descendants() returns an array
-//       // of all elements in the hierarchy with
-//       // parent and children methods.
-//       // The first element is not necessary
-//       // because we can infer the x and y
-//       // coordinates from the parent attribute
-//       // of the first children
-//       .data(root.descendants().slice(1))
-//       .enter().append("path")
-//       .attr("class", "link")
-//       .attr("d", function(d) {
-//         return "M" + d.y + "," + d.x
-//             + "C" + (d.parent.y + 100) + "," + d.x
-//             + " " + (d.parent.y + 100) + "," + d.parent.x
-//             + " " + d.parent.y + "," + d.parent.x;
-//       });
-
-//   // Create g element which holds
-//   // the circles and texts
-//   var node = g.selectAll(".node")
-//       .data(root.descendants())
-//       .enter().append("g")
-//       .attr("class", function(d) {
-//         // If node has children assign a different
-//         // class to the g element
-//         return "node" + (d.children ? " node--internal" : " node--leaf");
-//       })
-//       .attr("transform", function(d) {
-//         return "translate(" + d.y + "," + d.x + ")";
-//       });
-
-//   // Append circles
-//   node.append("circle")
-//       .attr("r", 2.5);
-
-//   // Append text
-//   node.append("text")
-//       .attr("dy", 5)
-//       .attr("x", function(d) {
-//         // If element has children change the
-//         // relative x coordinate
-//         return d.children ? -8 : 8;
-//       })
-//       .style("text-anchor", function(d) {
-//         // If element has children anchor text
-//         // and the end. If not at the start
-//         return d.children ? "end" : "start";
-//       })
-//       .text(function(d) {
-//         return d.data.name;
-//       });
-// });
-
-
-
-
-
-
 const width = window.innerWidth,
     height = window.innerHeight,
     maxRadius = (Math.min(width, height) / 2) - 5;
@@ -134,10 +42,6 @@ const textFits = d => {
     const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
     const perimeter = r * deltaAngle;
 
-    // console.log(d)
-    // console.log(d.data)
-    // console.log(d.data.name)
-    // console.log(d.name)
     return d.data.name.length * CHAR_SPACE < perimeter;
 };
 
@@ -149,7 +53,7 @@ const svg = d3.select('body').append('svg')
 
 d3.json('https://raw.githubusercontent.com/imogencs/04-multiple-views/main/animale22.json', (error, root) => {
     if (error) throw error;
-    // console.log(root)
+    console.log(root)
 
     root = d3.hierarchy(root);
     root.sum(d => d.size);
@@ -158,7 +62,6 @@ d3.json('https://raw.githubusercontent.com/imogencs/04-multiple-views/main/anima
         .data(partition(root).descendants());
 
     slice.exit().remove();
-
 
     const newSlice = slice.enter()
         .append('g').attr('class', 'slice')
@@ -170,25 +73,19 @@ d3.json('https://raw.githubusercontent.com/imogencs/04-multiple-views/main/anima
     newSlice.append('title')
         .text(d => d.data.name + '\n' + formatNumber(d.value));
 
-
-    // console.log("here 1")
     newSlice.append('path')
         .attr('class', 'main-arc')
-        // .style('fill', d => color((d.children ? d : d.parent).name))
-        .style('fill', function () {
-            return "hsl(" + Math.random() * 360 + ",100%,50%)";
-        })
+        .style('fill', d => color((d.children ? d : d.parent).data.name))
         .attr('d', arc);
-    // console.log("here 2")
 
     newSlice.append('path')
         .attr('class', 'hidden-arc')
         .attr('id', (_, i) => `hiddenArc${i}`)
         .attr('d', middleArcLine);
-    // console.log("here 3")
+
     const text = newSlice.append('text')
         .attr('display', d => textFits(d) ? null : 'none');
-    // console.log("here 4")
+
     // Add white contour
     text.append('textPath')
         .attr('startOffset', '50%')
@@ -199,7 +96,6 @@ d3.json('https://raw.githubusercontent.com/imogencs/04-multiple-views/main/anima
         .style('stroke-width', 5)
         .style('stroke-linejoin', 'round');
 
-    console.log("here 5")
     text.append('textPath')
         .attr('startOffset', '50%')
         .attr('xlink:href', (_, i) => `#hiddenArc${i}`)
